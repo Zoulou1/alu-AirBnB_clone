0f5e7689802576becca1e6a43ed27bd63cd70322
mikarurangwa <r.mika@alustudent.com>
2025-01-17 11:36:40 +0200

    update

--- models/city.py
+++ models/city.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""Defines the city class"""
+"""City class"""
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Represents a city.
-    Attributes:
-        state_id (str): The state id.
-        name (str): The name of the city.
-    """
-
-    state_id = ""
+    """City class"""
     name = ""
+    state_id = ""
+    """initialise class"""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a City instance. If given, the arguments are used to set the
+        instance's attributes. Otherwise, the instance's attributes are set to
+        empty strings.
+        """
+        super().__init__(*args, **kwargs)
