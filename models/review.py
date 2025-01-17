9234a85a06c870c9b95c9a474e77115df739e135
mikarurangwa <r.mika@alustudent.com>
2025-01-17 11:37:47 +0200

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Defines the Review class."""
+"""Review class"""
+
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Represent a review.
-
-    Attributes:
-        place_id (str): The Place id.
-        user_id (str): The User id.
-        text (str): The text of the review.
     """
-
+    Review class
+    """
     place_id = ""
     user_id = ""
     text = ""
+
+    def __init__(self, *args, **kwargs):
+        """initialise class"""
+        super().__init__(*args, **kwargs)
